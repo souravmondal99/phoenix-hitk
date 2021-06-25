3705f3fb79b38cf54b74894823682be57e6e9a17
Sourav Mondal <81108325+souravmondal99@users.noreply.github.com>
2021-06-25 19:53:36 +0530

    Update Loginservlet.java

--- src/main/java/servlet/Loginservlet.java
+++ src/main/java/servlet/Loginservlet.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Properties;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
